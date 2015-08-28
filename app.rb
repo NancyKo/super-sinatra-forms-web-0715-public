c9c53dd2cf41d0dff5ebb9b26cb56c3eef055776
NancyKo <konancys@gmail.com>
2015-08-28 16:24:01 -0400

    done

--- app.rb
+++ app.rb
@@ -1,12 +1,5 @@
 require 'sinatra/base'
 
-class Team
-	attr_accessor :name, :motto
-
-	def initialize(params)
-		@params = params
-	end
-end
 
 class Superhero
 	attr_accessor :name, :power, :biography
@@ -16,7 +9,16 @@ class Superhero
 	end
 end
 
+class Team
+	attr_accessor :name, :motto
+
+	def initialize(params)
+		@params = params
+	end
+end
+
 class App < Sinatra::Base
+
 	get '/' do
 		erb :'/super_hero'
 	end
@@ -25,6 +27,23 @@ class App < Sinatra::Base
 		@team = Team.new(params)
 		@team.name = params["team"]["name"]
 		@team.motto = params["team"]["motto"]
-		erb :'/team'
+
+		@hero = Superhero.new(params)
+		@hero.name = params["team"]["members"]["super1"]["name"]
+		@hero.power = params["team"]["members"]["super1"]["power"]
+		@hero.biography = params["team"]["members"]["super1"]["biography"]
+
+		@heroo = Superhero.new(params)
+		@heroo.name = params["team"]["members"]["super2"]["name"]
+		@heroo.power = params["team"]["members"]["super2"]["power"]
+		@heroo.biography = params["team"]["members"]["super2"]["biography"] 
+
+
+		@herooo = Superhero.new(params)
+		@herooo.name = params["team"]["members"]["super3"]["name"]
+		@herooo.power = params["team"]["members"]["super3"]["power"]
+		@herooo.biography = params["team"]["members"]["super3"]["biography"] 
+
+		erb :'/teams_show'
 	end
 end
