7d9761c87ded649e4492341aaca43e6263e597c9
N K <konancys@gmail.com>
2015-08-28 00:49:22 -0400

    Adding team view

--- app.rb
+++ app.rb
@@ -1,13 +1,30 @@
 require 'sinatra/base'
 
+class Team
+	attr_accessor :name, :motto
+
+	def initialize(params)
+		@params = params
+	end
+end
+
+class Superhero
+	attr_accessor :name, :power, :biography
+
+	def initialize(params)
+		@params = params
+	end
+end
+
 class App < Sinatra::Base
-
-
 	get '/' do
 		erb :'/super_hero'
 	end
 
 	post '/teams' do
+		@team = Team.new(params)
+		@team.name = params["team"]["name"]
+		@team.motto = params["team"]["motto"]
 		erb :'/team'
 	end
 end
